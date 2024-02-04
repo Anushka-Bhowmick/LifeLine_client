e3711fb87a267d3d8ad0994ddd41a0d9bac5fb98
Ashish Gupta <gupta.ashish2694@gmail.com>
2024-02-04 11:59:47 +0530

    Sign_UP_test

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -3,6 +3,7 @@ import Chatbot from './components/Chatbot'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import { signInWithGoogle } from "./firebase"
+import '../src/components/Hero.css'
 function App() {
 
   return (
@@ -10,7 +11,7 @@ function App() {
     
     <Header/>
     <div>
-    <button onClick = {signInWithGoogle}>Sign In With Google</button>
+        <button onClick = {signInWithGoogle} className='hero-btn secondary-btn'>Sign it with Google</button>
     </div>
     <Hero/>
       <Chatbot/>
